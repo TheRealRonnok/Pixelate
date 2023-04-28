3b4fcca1dc22a1d1fd2ecadf110d25ab832c423e
Ronnok <konnor.millerson@hotmail.com>
2023-04-27 21:32:32 -0500

    Added Switching Colors sections 1 and 2

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const table = document.getElementsByTagName('table')[0]
 const addRow = document.getElementById('add-row')
+const changeColor = document.getElementById('selectMenu')
+
+let chosenColor = 'red'
 
 function makeRow () {
     const row = document.createElement('tr')
@@ -9,16 +12,23 @@ function makeRow () {
     }
     table.appendChild(row)
 }
-
+// Make a row by clicking the Add Row button
 addRow.addEventListener("click", makeRow)
 
+// Color a cell by clicking it
 table.addEventListener('click', colorize)
 
+// Display the color that was selected in the Console
+changeColor.addEventListener('change', function(event) {
+  chosenColor = event.target.value;
+});
+
+// Color a cell
 function colorize (event) {
    const target = event.target
    if (target.className.length) {
     target.className = ''
   } else {
-    target.className = 'red'
+    target.className = chosenColor;
   }
 }
