5fd4b45a9edc21338d38b8be916346c94eac9a20
BullFrog <camroth2002@gmail.com>
2023-04-27 19:51:22 -0600

    letter fix

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function makeRow () {
     const row = document.createElement('tr')
     for (let i = 0; i < 20; i++) {
         const td = document.createElement('td')
-        row.appentChild(td)
+        row.appendChild(td)
     }
     table.appendChild(row)
 }
